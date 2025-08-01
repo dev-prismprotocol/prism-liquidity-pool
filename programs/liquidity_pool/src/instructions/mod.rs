246e6fd355b8b51cd230f250ff54238df132f8c7
Prism Protocol <dev.prismprotocol@gmail.com>
2025-08-01 09:06:53 +0700

    Update mod.rs

--- programs/liquidity_pool/src/instructions/mod.rs
+++ programs/liquidity_pool/src/instructions/mod.rs
@@ -1,5 +1,6 @@
 // programs/liquidity_pool/src/instructions/mod.rs
 pub mod create_pool;
+pub mod create_pool_token_account;
 pub mod swap;
 pub mod admin;
 pub mod query;
@@ -7,6 +8,7 @@ pub mod add_liquidity;
 pub mod remove_liquidity;
 
 pub use create_pool::*;
+pub use create_pool_token_account::*;
 pub use swap::*;
 pub use admin::*;
 pub use query::*;
